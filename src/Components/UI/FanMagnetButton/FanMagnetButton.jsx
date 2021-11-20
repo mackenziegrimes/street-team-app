7c899d80f2a7457ca5c1d347e11bba8bc018d617
modernmusician <michael@modernmusician.me>
2021-11-20 10:13:04 -0500

    Tweaked padding on Fan Feedback buttons

--- src/Components/UI/FanMagnetButton/FanMagnetButton.jsx
+++ src/Components/UI/FanMagnetButton/FanMagnetButton.jsx
@@ -23,7 +23,8 @@ const StyledButtonInner = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
+  width: auto;
+  margin-left: 25px;
   span {
     margin-right: 18px;
 
