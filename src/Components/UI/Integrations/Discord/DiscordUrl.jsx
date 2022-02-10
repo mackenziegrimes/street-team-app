c5056f27b4af20ecae61de7de4c62bf5f34ed4b1
modernmusician <michael@modernmusician.me>
2022-02-10 15:46:45 -0500

    changed Discord oauth url scopes

--- src/Components/UI/Integrations/Discord/DiscordUrl.jsx
+++ src/Components/UI/Integrations/Discord/DiscordUrl.jsx
@@ -1,17 +1,21 @@
-const authEndpoint = 'https://zoom.us/oauth/authorize';
-const clientId = '0QgLw4cgQOSmrxY6hdfnFw'; // this is our dev clientId
+const authEndpoint = 'https://discord.com/api/oauth2/authorize';
+const clientId = '886234811616882699'; // this is our clientId
 
 // build the current url to be used for oauth redirect (should probably use env variables... but this is quicker right now)
 const currentUrl = window.location.href;
 // takes the current url root, and adds the login route (which should get us back to the right page)
-const redirectUri = currentUrl.split('/').slice(0, 3).join('/') + '/login/zoom'; // zoom here is a dynamic service parameter
+const redirectUri = currentUrl.split('/').slice(0, 3).join('/') + '/login/discord'; // zoom here is a dynamic service parameter
 
-//for scope definition, see https://developer.zoom.com/documentation/general/guides/authorization/scopes/
+//for scope definition, see https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
 const scopes = [
-  "meeting:write:admin",
-  "meeting:read:admin"
+  "guilds.join",
+  "guilds",
+  "guilds.members.read",
+  "bot",
 ];
 
-export const loginUrl = `https://discord.com/api/oauth2/authorize?client_id=886234811616882699&permissions=8&scope=bot`;
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&permissions=8&redirect_uri=${redirectUri}&response_type=code&scope=${scopes}`;
+
+
 
 // loginUrl = "https://zoom.us/oauth/authorize?response_type=code&client_id=0QgLw4cgQOSmrxY6hdfnFw&redirect_uri=https://dev.modern-musician.com/login/zoom"
