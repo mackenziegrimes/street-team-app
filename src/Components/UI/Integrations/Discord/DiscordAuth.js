dd72f41275fcbc2824f04074657fb98b3e881f44
modernmusician <michael@modernmusician.me>
2022-02-10 12:40:48 -0500

    Copied and replaced Zoom auth integration

---
+++ src/Components/UI/Integrations/Discord/DiscordAuth.js
@@ -0,0 +1,57 @@
+import { getBackendApiUrl } from "../../../../utils/sharedUtils";
+
+export const handleDiscordAuth = async (authCode) => {
+    console.log(`auth code is `, authCode);
+    //handle the auth code from the discord redirect by calling our backend api
+    //1. call our backend api with the auth code to get our access token
+    //2. store that access token in the cookies
+    if(authCode){
+    const postUrl = getBackendApiUrl() + `/discord-auth`;
+    try{
+      const response = await fetch(postUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({auth_code: authCode}),
+      });
+      const responseJson = await response.json();
+      const {accessToken,refreshToken} = responseJson;
+      console.log(`discord access token is ${accessToken}`);
+      console.log(`discord refresh token is ${refreshToken}`);
+      //2. store that access token in the cookies
+      //this is useful because we don't necessarily have the enduserId at this point (the enduser might not be logged in to SteetTeam), so we're going to stash it for sending to our backend later
+      if(refreshToken){
+        console.log('yes! we have value');
+        window.localStorage.setItem('discordAccessToken', accessToken);
+        window.localStorage.setItem('discordRefreshToken', refreshToken);
+      }
+      }
+    catch(err){
+        console.error(`getting discord auth failed due to the following error:`);
+        console.error(err);
+    }
+    }
+    else{
+      console.log('handleDiscordAuth called with no authCode parameter')
+    }
+    return 'done';
+}
+
+export const saveDiscordAuth = async (authToken, enduserId) => {
+    console.log(`saving discord auth token:`,authToken, `for enduserId`,enduserId);
+    const postUrl = getBackendApiUrl() + `/discord-enduser-integration`;
+    try{
+        const response = await fetch(postUrl, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({service: "discord", token: authToken, enduserId: enduserId}),
+        });
+        const responseJson = await response.json();
+        const accessToken = responseJson.accessToken;
+        console.log(responseJson);
+    }
+    catch(err){
+          console.error(`getting discord auth failed due to the following error:`);
+          console.error(err);
+    }
+    return 'done';
+}
---
+++ src/Components/UI/Integrations/Discord/DiscordUrl.jsx
@@ -0,0 +1,17 @@
+const authEndpoint = 'https://zoom.us/oauth/authorize';
+const clientId = '0QgLw4cgQOSmrxY6hdfnFw'; // this is our dev clientId
+
+// build the current url to be used for oauth redirect (should probably use env variables... but this is quicker right now)
+const currentUrl = window.location.href;
+// takes the current url root, and adds the login route (which should get us back to the right page)
+const redirectUri = currentUrl.split('/').slice(0, 3).join('/') + '/login/zoom'; // zoom here is a dynamic service parameter
+
+//for scope definition, see https://developer.zoom.com/documentation/general/guides/authorization/scopes/
+const scopes = [
+  "meeting:write:admin",
+  "meeting:read:admin"
+];
+
+export const loginUrl = `https://discord.com/api/oauth2/authorize?client_id=886234811616882699&permissions=8&scope=bot`;
+
+// loginUrl = "https://zoom.us/oauth/authorize?response_type=code&client_id=0QgLw4cgQOSmrxY6hdfnFw&redirect_uri=https://dev.modern-musician.com/login/zoom"
